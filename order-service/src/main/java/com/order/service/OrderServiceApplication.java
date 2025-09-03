c3123741bcd26698270cfbed20323c85a666fd5f
MsbArjun <42497304+161683@users.noreply.github.com>
2025-09-03 22:45:37 +0530

    changes

--- order-service/src/main/java/com/order/service/OrderServiceApplication.java
+++ order-service/src/main/java/com/order/service/OrderServiceApplication.java
@@ -1,13 +1,13 @@
-package com.user_service;
+package com.order.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserServiceApplication {
+public class OrderServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
+		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
 }
--- user-service/src/main/java/com/user/service/UserServiceApplication.java
+++ user-service/src/main/java/com/user/service/UserServiceApplication.java
@@ -1,13 +1,13 @@
-package com.order_service;
+package com.user.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrderServiceApplication {
+public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrderServiceApplication.class, args);
+		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
 }
